80a163002a6290c45d3ad94c363893bb3f039844
Maddie <maddiemcampbell@gmail.com>
2019-11-13 12:00:15 -0800

    / 200 object status, and responds with all items in the dataset

--- server/app.js
+++ server/app.js
@@ -1,8 +1,41 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(morgan('dev'));
 
-// add your code here
+let tasks = [{
+    todoItemId: 0,
+    name: 'an item',
+    priority: 3,
+    completed: false
+  },
+  {
+    todoItemId: 1,
+    name: 'another item',
+    priority: 2,
+    completed: false
+  },
+  {
+    todoItemId: 2,
+    name: 'a done item',
+    priority: 1,
+    completed: true
+  }
+];
+
+app.get('/', function (req, res) {
+  res.status(200).send({
+    status: 'ok'
+  });
+});
+
+
+app.get('/api/TodoItems', function (req, res) {
+    console.log('req.params.number', req.params.number);
+  res.status(200).send(tasks);
+});
+
 
 module.exports = app;
